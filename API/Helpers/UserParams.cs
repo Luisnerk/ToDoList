401f825d9a080fde5f8aee844ea2333b06b1e95c
Luisnerk <luis_calix@live.com>
2026-01-28 16:11:19 -0600

    Added UserParams file on helpers

---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers;
+public class UserParams
+{
+    private const int _maxPageSize = 20;
+    public int pageNumber;
+    private int _pageSize;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+    }
+}
